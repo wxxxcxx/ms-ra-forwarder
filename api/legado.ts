4d2d34c92e1a5f8b7f80d2b32bcd359d6d4394a6
rogueme <37013377+rogueme@users.noreply.github.com>
2022-05-19 23:57:26 +0800

    Update legado.ts

--- api/legado.ts
+++ api/legado.ts
@@ -3,7 +3,7 @@ import { FORMAT_CONTENT_TYPE } from '../ra';
 import { encode, decode } from '../base64/base64';
 module.exports = async (request: VercelRequest, response: VercelResponse) => {
     let jsondatastr = decode(request.query['json']);
-    let jsondata = json.parse(jsondatastr);
+    let jsondata = JSON.parse(jsondatastr);
     let api = jsondata['url'] ?? '';
     let name = jsondata['name'] ?? '大声朗读';
     let voiceName = jsondata['voiceName'] ?? 'zh-CN-XiaoxiaoNeural';
