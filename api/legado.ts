f8868bfc08046730dbdee8ff6fdf5c2e4da47cb3
meetcw <meetcw@outlook.com>
2022-07-12 01:12:21 +0800

    adjust speak speed

--- api/legado.ts
+++ api/legado.ts
@@ -34,7 +34,7 @@ module.exports = async (request: Request, response: Response) => {
   let ssml =
     '<speak xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="http://www.w3.org/2001/mstts" xmlns:emo="http://www.w3.org/2009/10/emotionml" version="1.0" xml:lang="en-US">' +
       '<voice name="' +voiceName +'">' +
-        '<prosody rate="{{speakSpeed * 2}}%" pitch="+0Hz">' +
+        '<prosody rate="{{(speakSpeed - 10) * 2}}%" pitch="+0Hz">' +
           "{{String(speakText).replace(/&/g, '&amp;').replace(/\"/g, '&quot;').replace(/'/g, '&apos;').replace(/</g, '&lt;').replace(/>/g, '&gt;')}}" +
         '</prosody>' +
       '</voice>' +
