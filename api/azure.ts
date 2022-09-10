4986e6e35805e81405243c8033e140f7472684ab
meetcw <meetcw@outlook.com>
2022-09-10 16:53:05 +0800

    retry when convert error

--- api/azure.ts
+++ api/azure.ts
@@ -2,12 +2,13 @@ import axios from 'axios'
 import { Request, Response } from 'express'
 import e = require('express')
 import { service, FORMAT_CONTENT_TYPE } from '../service/azure'
+import { retry } from '../retry'
 
 module.exports = async (request: Request, response: Response) => {
   try {
     if (request.method === 'GET') {
       let listResponse = await axios.get(
-        'https://eastus.api.speech.microsoft.com/cognitiveservices/voices/list'
+        'https://eastus.api.speech.microsoft.com/cognitiveservices/voices/list',
       )
       let data = listResponse.data
       response
@@ -39,15 +40,25 @@ module.exports = async (request: Request, response: Response) => {
       if (ssml == null) {
         throw `转换参数无效`
       }
-      let result = await service.convert(ssml, format)
+      let result = await retry(
+        async () => {
+          let result = await service.convert(ssml, format as string)
+          return result
+        },
+        3,
+        (index, error) => {
+          console.warn(`第${index}次转换失败：${error}`)
+        },
+        '服务器多次尝试后转换失败',
+      )
       response.sendDate = true
       response
         .status(200)
-        .setHeader('Content-Type', FORMAT_CONTENT_TYPE.get(format))
+        .setHeader('Content-Type', FORMAT_CONTENT_TYPE.get(format as string))
       response.end(result)
     }
   } catch (error) {
-    console.error('发生错误', error.message)
+    console.error(`发生错误, ${error.message}`)
     response.status(503).json(error)
   }
 }
