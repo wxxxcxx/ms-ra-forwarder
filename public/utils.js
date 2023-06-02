e3dda08829413fcd0b9260e4432aafa9f6ce1ab3
itodouble <itodouble@163.com>
2023-06-02 15:49:20 +0800

    优化中文显示

---
+++ public/utils.js
@@ -0,0 +1,124 @@
+const languageCode = {
+    "af": {code: "af", name: "Afrikaans", nameCn: "南非荷兰语"},
+    "am": {code: "am", name: "Amharic", nameCn: "埃塞俄比亚"},
+    "ar": {code: "ar", name: "Arabic", nameCn: "阿拉伯语"},
+    "az": {code: "az", name: "Azeri", nameCn: "阿塞拜疆语"},
+    "be": {code: "be", name: "Belarusian", nameCn: "白俄罗斯语"},
+    "bn": {code: "bn", name: "Bengali", nameCn: "孟加拉语"},
+    "bs": {code: "bs", name: "Bosnian", nameCn: "波斯尼亚语"},
+    "bg": {code: "bg", name: "Bulgarian", nameCn: "保加利亚语"},
+    "ca": {code: "ca", name: "Catalan", nameCn: "加泰罗尼亚语"},
+    "cs": {code: "cs", name: "Czech", nameCn: "捷克语"},
+    "cy": {code: "cy", name: "Welsh", nameCn: "威尔士语"},
+    "da": {code: "da", name: "Danish", nameCn: "丹麦语"},
+    "de": {code: "de", name: "German", nameCn: "德语"},
+    "dv": {code: "dv", name: "Divehi", nameCn: "迪维希语"},
+    "el": {code: "el", name: "Greek", nameCn: "希腊语"},
+    "en": {code: "en", name: "English", nameCn: "英语"},
+    "eo": {code: "eo", name: "Esperanto", nameCn: "世界语"},
+    "es": {code: "es", name: "Spanish", nameCn: "西班牙语"},
+    "et": {code: "et", name: "Estonian", nameCn: "爱沙尼亚语"},
+    "eu": {code: "eu", name: "Basque", nameCn: "巴斯克语"},
+    "fa": {code: "fa", name: "Farsi", nameCn: "波斯语"},
+    "fi": {code: "fi", name: "Finnish", nameCn: "芬兰语"},
+    "fo": {code: "fo", name: "Faroese", nameCn: "法罗群岛语"},
+    "fr": {code: "fr", name: "French", nameCn: "法语"},
+    "ga":{code:"ga",name:"Irish",nameCn:"爱尔兰语"},
+    "gl": {code: "gl", name: "Galician", nameCn: "加利西亚语"},
+    "gu": {code: "gu", name: "Gujarati", nameCn: "古吉拉特语"},
+    "he": {code: "he", name: "Hebrew", nameCn: "希伯来语"},
+    "hi": {code: "hi", name: "Hindi", nameCn: "印地语"},
+    "hr": {code: "hr", name: "Croatian", nameCn: "克罗地亚语"},
+    "hu": {code: "hu", name: "Hungarian", nameCn: "匈牙利语"},
+    "hy": {code: "hy", name: "Armenian", nameCn: "亚美尼亚语"},
+    "id": {code: "id", name: "Indonesian", nameCn: "印尼语"},
+    "is": {code: "is", name: "Icelandic", nameCn: "冰岛语"},
+    "it": {code: "it", name: "Italian", nameCn: "意大利语"},
+    "ja": {code: "ja", name: "Japanese", nameCn: "日语"},
+    "jv":{code:"jv",name:"Javanese",nameCn:"爪哇语"},
+    "ka": {code: "ka", name: "Georgian", nameCn: "格鲁吉亚语"},
+    "kk": {code: "kk", name: "Kazakh", nameCn: "哈萨克语"},
+    "kn":{code:"kn",name:"Kannada",nameCn:"卡纳达语"},
+    "km":{code:"km",name:"Khmer",nameCn:"高棉语"},
+    "ko": {code: "ko", name: "Korean", nameCn: "韩语"},
+    "kok": {code: "kok", name: "Konkani", nameCn: "孔卡尼语"},
+    "ky": {code: "ky", name: "Kyrgyz", nameCn: "吉尔吉斯语"},
+    "lo":{code:"lo",name:"Lao",nameCn:"老挝语"},
+    "lt": {code: "lt", name: "Lithuanian", nameCn: "立陶宛语"},
+    "lv": {code: "lv", name: "Latvian", nameCn: "拉脱维亚语"},
+    "mi": {code: "mi", name: "Maori", nameCn: "毛利语"},
+    "mk": {code: "mk", name: "FYRO Macedonian", nameCn: "马其顿文"},
+    "ml":{code:"ml",name:"Malayalam",nameCn:"马拉雅拉姆语"},
+    "mn": {code: "mn", name: "Mongolian", nameCn: "蒙古语"},
+    "mr": {code: "mr", name: "Marathi", nameCn: "马拉地语"},
+    "ms": {code: "ms", name: "Malay", nameCn: "马来语"},
+    "mt": {code: "mt", name: "Maltese", nameCn: "马耳他语"},
+    "my":{code:"my",name:"Burmese",nameCn:"缅甸语"},
+    "nb": {code: "nb", name: "Norwegian", nameCn: "挪威语"},
+    "ne":{code:"ne",name:"Nepali",codeCn:"尼泊尔语"},
+    "nl": {code: "nl", name: "Dutch", nameCn: "荷兰语"},
+    "ns": {code: "ns", name: "Northern Sotho", nameCn: "北索托语"},
+    "pl": {code: "pl", name: "Polish", nameCn: "波兰语"},
+    "ps": {code: "ps", name: "Pashto", nameCn: "普什图语"},
+    "pt": {code: "pt", name: "Portuguese", nameCn: "葡萄牙语"},
+    "qu": {code: "qu", name: "Quechua", nameCn: "克丘亚语"},
+    "ro": {code: "ro", name: "Romanian", nameCn: "罗马尼亚语"},
+    "ru": {code: "ru", name: "Russian", nameCn: "俄语"},
+    "sa": {code: "sa", name: "Sanskrit", nameCn: "梵语"},
+    "se": {code: "se", name: "Sami", nameCn: "萨米语"},
+    "si":{code:"si",name:"Sinhala",nameCn:"僧伽罗语"},
+    "sk": {code: "sk", name: "Slovak", nameCn: "斯洛伐克语"},
+    "sl": {code: "sl", name: "Slovenian", nameCn: "斯洛文尼亚语"},
+    "so":{code:"so",name:"Somali",nameCn:"索马里语"},
+    "sq": {code: "sq", name: "Albanian", nameCn: "阿尔巴尼亚语"},
+    "sr":{code:"sr",name:"Serbian",nameCn:"塞尔维亚语"},
+    "su":{code:"su",name:"Sundanese",nameCn:"巽他语"},
+    "sv": {code: "sv", name: "Swedish", nameCn: "瑞典语"},
+    "sw": {code: "sw", name: "Swahili", nameCn: "斯瓦希里语"},
+    "syr": {code: "syr", name: "Syriac", nameCn: "叙利亚语"},
+    "ta": {code: "ta", name: "Tamil", nameCn: "泰米尔语"},
+    "te": {code: "te", name: "Telugu", nameCn: "泰卢固语"},
+    "th": {code: "th", name: "Thai", nameCn: "泰语"},
+    "tl": {code: "tl", name: "Tagalog", nameCn: "他加禄语"},
+    "tn": {code: "tn", name: "Tswana", nameCn: "茨瓦纳语"},
+    "tr": {code: "tr", name: "Turkish", nameCn: "土耳其语"},
+    "tt": {code: "tt", name: "Tatar", nameCn: "鞑靼语"},
+    "uk": {code: "uk", name: "Ukrainian", nameCn: "乌克兰语"},
+    "ur": {code: "ur", name: "Urdu", nameCn: "乌尔都语"},
+    "uz": {code: "uz", name: "Uzbek", nameCn: "乌兹别克语"},
+    "vi": {code: "vi", name: "Vietnamese", nameCn: "越南语"},
+    "xh": {code: "xh", name: "Xhosa", nameCn: "科萨语"},
+    "zh": {code: "zh", name: "Chinese", nameCn: "中文"},
+    "zu": {code: "zu", name: "Zulu", nameCn: "祖鲁语"},
+}
+
+function getLanguageLocalInfoByCode(code) {
+    return languageCode[code];
+}
+
+function getLanguageLocalNameCn(code) {
+    let localInfo = getLanguageLocalInfoByCode(code.split('-')[0]);
+    if (null != localInfo) {
+        return localInfo['nameCn'];
+    }
+    return code;
+}
+
+function getSex(code) {
+    if (code === 'Female') {
+        return '女性';
+    } else if (code === 'Male') {
+        return '男性';
+    } else {
+        return code;
+    }
+}
+
+function friendlyName(name){
+    try {
+        const result = name.match(/^Microsoft (\w*) Online \((\w*)\) - (\w*) \((.*)\)/)
+        return result[1] + "(" + result[4]+")"
+    } catch (e) {
+        return name;
+    }
+}
