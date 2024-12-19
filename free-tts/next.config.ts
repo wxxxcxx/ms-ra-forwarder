064efdd36d76d146d1345d06b59f7f4e6cf02802
W <wxxxcxx@foxmail.com>
2024-12-19 18:58:36 +0800

    refactor: 使用Nextjs重构

---
+++ free-tts/next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  i18n: {
+    locales: ["en-US", "zh-CN"],
+    defaultLocale: "en-US",
+  },
+};
+
+export default nextConfig;
